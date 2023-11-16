4bd6cf5949f501f0303787526e245af85472c2c0
Mark Mesander <mark_mesander@hotmail.com>
2023-11-16 15:52:26 +0100

    create Television

--- src/main/java/mesander/com/TechItEasy/controllers/TelevisionController.java
+++ src/main/java/mesander/com/TechItEasy/controllers/TelevisionController.java
@@ -1,8 +1,24 @@
 package mesander.com.TechItEasy.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class TelevisionController {
 
+
+
+    @GetMapping("/televisions")
+    public String getAllTelevisions() {
+        return "Dit is een lijst met alle televisies";
+    }
+
+    @PostMapping("/televisions")
+    public void postTelevision(@RequestParam String television) {
+
+    }
 }
