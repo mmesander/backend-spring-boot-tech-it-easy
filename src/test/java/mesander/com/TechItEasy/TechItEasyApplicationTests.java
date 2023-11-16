f0e20653c1b64dfe6e0c7c3caa7ddfe5d045f9c8
Mark Mesander <mark_mesander@hotmail.com>
2023-11-16 13:53:09 +0100

    first commit

---
+++ src/test/java/mesander/com/TechItEasy/TechItEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package mesander.com.TechItEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
