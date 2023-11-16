4a5045722c79cc725a5f2e14a38619e917570d1e
Mark Mesander <mark_mesander@hotmail.com>
2023-11-16 17:23:27 +0100

    add ExceptionController

--- src/main/java/mesander/com/TechItEasy/controllers/ExceptionController.java
+++ src/main/java/mesander/com/TechItEasy/controllers/ExceptionController.java
@@ -1,5 +1,15 @@
 package mesander.com.TechItEasy.controllers;
 
-public class ExceptionController {
+import mesander.com.TechItEasy.exceptions.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<Object> exception(RecordNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
