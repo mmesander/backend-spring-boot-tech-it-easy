7d5ad39812469d1d48e1324c37c087bdf6fbc4ad
Mark Mesander <mark_mesander@hotmail.com>
2023-11-23 16:59:20 +0100

    update television controller, interact with database

--- src/main/java/mesander/com/TechItEasy/controllers/TelevisionController.java
+++ src/main/java/mesander/com/TechItEasy/controllers/TelevisionController.java
@@ -2,53 +2,74 @@ package mesander.com.TechItEasy.controllers;
 
 import mesander.com.TechItEasy.models.Television;
 import mesander.com.TechItEasy.exceptions.RecordNotFoundException;
-import org.springframework.http.HttpStatus;
+import mesander.com.TechItEasy.repositories.TelevisionRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TelevisionController {
-    private ArrayList<Television> televisions = new ArrayList<>();
+    private final TelevisionRepository televisionRepository;
+
+    public TelevisionController(TelevisionRepository televisionRepository) {
+        this.televisionRepository = televisionRepository;
+    }
 
     @GetMapping("/televisions")
-    public ResponseEntity<ArrayList<Television>> getAllTelevisions() {
-        return new ResponseEntity<>(this.televisions, HttpStatus.OK);
+    public ResponseEntity<List<Television>> getAllTelevisions(@RequestParam(value = "brand", required = false) String brand) {
+        List<Television> televisions;
+        televisions = televisionRepository.findAll();
+        return ResponseEntity.ok().body(televisions);
     }
 
     @GetMapping("/televisions/{id}")
-    public ResponseEntity<Television> getTelevision(@PathVariable int id) {
-        if (id >= 0 && id < this.televisions.size()) {
-            return new ResponseEntity<>(this.televisions.get(id), HttpStatus.OK);
-        } else {
-            throw new RecordNotFoundException("ID cannot be found");
-        }
-    }
+    public ResponseEntity<Television> getSpecificTelevision(@PathVariable("id") Long id) {
+        Optional<Television> television = televisionRepository.findById(id);
 
-    @PostMapping("/televisions")
-    public ResponseEntity<Television> addTelevision(@RequestBody Television television) {
-        this.televisions.add(television);
-        return new ResponseEntity<>(television, HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Television> updateTelevision(@PathVariable int id, @RequestBody Television television) {
-        if (id >= 0 && id < this.televisions.size()) {
-            this.televisions.set(id, television);
-            return new ResponseEntity<>(television, HttpStatus.OK);
+        if (television.isEmpty()) {
+            throw new RecordNotFoundException("No television found with id: " + id);
         } else {
-            throw new RecordNotFoundException("ID cannot be found");
+            Television foundTelevision = television.get();
+            return ResponseEntity.ok().body(foundTelevision);
         }
+
     }
 
     @DeleteMapping("/televisions/{id}")
-    public ResponseEntity<Television> deleteTelevision(@PathVariable int id) {
-        if (id >= 0 && id < this.televisions.size()) {
-            this.televisions.remove(id);
-            return ResponseEntity.noContent().build();
+    public ResponseEntity<Television> deleteSpecificTelevision(@PathVariable("id") Long id) {
+        televisionRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/televisions/{id}")
+    public ResponseEntity<Television> updateTelevision(@PathVariable("id") Long id, @RequestBody Television newTelevision) {
+        Optional<Television> television = televisionRepository.findById(id);
+
+        if (television.isEmpty()) {
+            throw new RecordNotFoundException("No television found with id: " + id);
         } else {
-            throw new RecordNotFoundException("ID cannot be found");
+            Television foundTelevision = television.get();
+            foundTelevision.setAmbiLight(newTelevision.getAmbiLight());
+            foundTelevision.setAvailableSize(newTelevision.getAvailableSize());
+            foundTelevision.setBluetooth(newTelevision.getBluetooth());
+            foundTelevision.setBrand(newTelevision.getBrand());
+            foundTelevision.setHdr(newTelevision.getHdr());
+            foundTelevision.setName(newTelevision.getName());
+            foundTelevision.setOriginalStock(newTelevision.getOriginalStock());
+            foundTelevision.setPrice(newTelevision.getPrice());
+            foundTelevision.setRefreshRate(newTelevision.getRefreshRate());
+            foundTelevision.setScreenQuality(newTelevision.getScreenQuality());
+            foundTelevision.setScreenType(newTelevision.getScreenType());
+            foundTelevision.setSmartTv(newTelevision.getSmartTv());
+            foundTelevision.setSold(newTelevision.getSold());
+            foundTelevision.setType(newTelevision.getType());
+            foundTelevision.setVoiceControl(newTelevision.getVoiceControl());
+            foundTelevision.setWifi(newTelevision.getWifi());
+
+            Television updatedTelevision = televisionRepository.save(foundTelevision);
+            return ResponseEntity.ok().body(updatedTelevision);
         }
     }
+
 }
