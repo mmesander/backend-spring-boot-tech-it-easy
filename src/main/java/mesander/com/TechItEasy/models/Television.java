35db9691c17d90f487deae8e10e84440fe541c2c
Mark Mesander <mark_mesander@hotmail.com>
2023-11-23 15:24:36 +0100

    update Television and port

--- src/main/java/mesander/com/TechItEasy/models/Television.java
+++ src/main/java/mesander/com/TechItEasy/models/Television.java
@@ -1,29 +1,84 @@
 package mesander.com.TechItEasy.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "televisions")
 public class Television {
     // Variables
-    private long serialNumber;
-    private String name;
-    private String brand;
-    private double price;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    @Column
+    private String type;
+    @Column
+    private String brand;
+    @Column
+    private String name;
+    @Column
+    private Double price;
+    @Column(name = "available_size")
+    private Double availableSize;
+    @Column(name = "refresh_rate")
+    private double RefreshRate;
+    @Column(name = "screen_type")
+    private String screenType;
+    @Column(name = "screen_quality")
+    private String screenQuality;
+    @Column(name = "smart_tv")
+    private Boolean smartTv;
+    @Column
+    private Boolean wifi;
+    @Column(name = "voice_control")
+    private Boolean voiceControl;
+    @Column
+    private Boolean hdr;
+    @Column
+    private Boolean bluetooth;
+    @Column(name = "ambi_light")
+    private Boolean ambiLight;
+    @Column(name = "original_stock")
+    private Integer originalStock;
+    @Column
+    private Integer sold;
 
     // Constructor
-    public Television(String name, String brand, long serialNumber, double price) {
-        this.name = name;
+    public Television(Long id, String type, String brand, String name, Double price, Double availableSize, double refreshRate, String screenType, String screenQuality, Boolean smartTv, Boolean wifi, Boolean voiceControl, Boolean hdr, Boolean bluetooth, Boolean ambiLight, Integer originalStock, Integer sold) {
+        this.id = id;
+        this.type = type;
         this.brand = brand;
-        this.serialNumber = serialNumber;
+        this.name = name;
         this.price = price;
+        this.availableSize = availableSize;
+        RefreshRate = refreshRate;
+        this.screenType = screenType;
+        this.screenQuality = screenQuality;
+        this.smartTv = smartTv;
+        this.wifi = wifi;
+        this.voiceControl = voiceControl;
+        this.hdr = hdr;
+        this.bluetooth = bluetooth;
+        this.ambiLight = ambiLight;
+        this.originalStock = originalStock;
+        this.sold = sold;
     }
-
 
     // Getters and Setters
-    public String getName() {
-        return name;
+    public Long getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getBrand() {
@@ -34,19 +89,115 @@ public class Television {
         this.brand = brand;
     }
 
-    public long getSerialNumber() {
-        return serialNumber;
+    public String getName() {
+        return name;
     }
 
-    public void setSerialNumber(long serialNumber) {
-        this.serialNumber = serialNumber;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public double getPrice() {
+    public Double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
+    public void setPrice(Double price) {
         this.price = price;
+    }
+
+    public Double getAvailableSize() {
+        return availableSize;
+    }
+
+    public void setAvailableSize(Double availableSize) {
+        this.availableSize = availableSize;
+    }
+
+    public double getRefreshRate() {
+        return RefreshRate;
+    }
+
+    public void setRefreshRate(double refreshRate) {
+        RefreshRate = refreshRate;
+    }
+
+    public String getScreenType() {
+        return screenType;
+    }
+
+    public void setScreenType(String screenType) {
+        this.screenType = screenType;
+    }
+
+    public String getScreenQuality() {
+        return screenQuality;
+    }
+
+    public void setScreenQuality(String screenQuality) {
+        this.screenQuality = screenQuality;
+    }
+
+    public Boolean getSmartTv() {
+        return smartTv;
+    }
+
+    public void setSmartTv(Boolean smartTv) {
+        this.smartTv = smartTv;
+    }
+
+    public Boolean getWifi() {
+        return wifi;
+    }
+
+    public void setWifi(Boolean wifi) {
+        this.wifi = wifi;
+    }
+
+    public Boolean getVoiceControl() {
+        return voiceControl;
+    }
+
+    public void setVoiceControl(Boolean voiceControl) {
+        this.voiceControl = voiceControl;
+    }
+
+    public Boolean getHdr() {
+        return hdr;
+    }
+
+    public void setHdr(Boolean hdr) {
+        this.hdr = hdr;
+    }
+
+    public Boolean getBluetooth() {
+        return bluetooth;
+    }
+
+    public void setBluetooth(Boolean bluetooth) {
+        this.bluetooth = bluetooth;
+    }
+
+    public Boolean getAmbiLight() {
+        return ambiLight;
+    }
+
+    public void setAmbiLight(Boolean ambiLight) {
+        this.ambiLight = ambiLight;
+    }
+
+    public Integer getOriginalStock() {
+        return originalStock;
+    }
+
+    public void setOriginalStock(Integer originalStock) {
+        this.originalStock = originalStock;
+    }
+
+    public Integer getSold() {
+        return sold;
+    }
+
+    public void setSold(Integer sold) {
+        this.sold = sold;
     }
 }
