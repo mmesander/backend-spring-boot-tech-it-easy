386e54f2857f215ce97a71b6ff9530d01730e433
Mark Mesander <mark_mesander@hotmail.com>
2023-12-12 11:24:55 +0100

    add CI-Module service

--- src/main/java/mesander/com/TechItEasy/services/CIModuleService.java
+++ src/main/java/mesander/com/TechItEasy/services/CIModuleService.java
@@ -2,9 +2,14 @@ package mesander.com.TechItEasy.services;
 
 import mesander.com.TechItEasy.dtos.input.CIModuleInputDto;
 import mesander.com.TechItEasy.dtos.output.CIModuleDto;
+import mesander.com.TechItEasy.exceptions.RecordNotFoundException;
 import mesander.com.TechItEasy.models.CIModule;
 import mesander.com.TechItEasy.repositories.CIModuleRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CIModuleService {
@@ -12,6 +17,111 @@ public class CIModuleService {
 
     public CIModuleService(CIModuleRepository ciModuleRepository) {
         this.ciModuleRepository = ciModuleRepository;
+    }
+
+
+    // CRUD Methods
+    public List<CIModuleDto> getAllCIModules() {
+        List<CIModule> ciModules = ciModuleRepository.findAll();
+        List<CIModuleDto> ciModuleDtos = new ArrayList<>();
+
+        for (CIModule ciModule : ciModules) {
+            CIModuleDto dto = transferToDto(ciModule);
+            ciModuleDtos.add(dto);
+        }
+
+        if (ciModuleDtos.isEmpty()) {
+            throw new RecordNotFoundException("No CI-modules found");
+        } else {
+            return ciModuleDtos;
+        }
+    }
+
+    public List<CIModuleDto> getAllCIModulesByName(String name) {
+        List<CIModule> ciModules = ciModuleRepository.findCIModuleByName(name);
+        List<CIModuleDto> ciModuleDtos = new ArrayList<>();
+
+        for (CIModule ciModule : ciModules) {
+            CIModuleDto dto = transferToDto(ciModule);
+            ciModuleDtos.add(dto);
+        }
+
+        if (ciModuleDtos.isEmpty()) {
+            throw new RecordNotFoundException("No CI-modules found with name: " + name);
+        } else {
+            return ciModuleDtos;
+        }
+    }
+
+    public CIModuleDto getCIModuleById(Long id) {
+        Optional<CIModule> ciModule = ciModuleRepository.findById(id);
+
+        if (ciModule.isPresent()) {
+            return transferToDto(ciModule.get());
+        } else {
+            throw new RecordNotFoundException("No CI-module found with ID: " + id);
+        }
+    }
+
+    public void deleteCIModule(Long id) {
+        Optional<CIModule> ciModule = ciModuleRepository.findById(id);
+
+        if (ciModule.isPresent()) {
+            ciModuleRepository.deleteById(id);
+        } else {
+            throw new RecordNotFoundException("No CI-module found with ID: " + id);
+        }
+    }
+
+    public CIModuleDto createCIModule(CIModuleInputDto inputDto) {
+        CIModule ciModule = transferToCIModule(inputDto);
+
+        ciModuleRepository.save(ciModule);
+
+        return transferToDto(ciModule);
+    }
+
+    public CIModuleDto updateCIModule(Long id, CIModuleInputDto inputDto) {
+        Optional<CIModule> ciModule = ciModuleRepository.findById(id);
+
+        if (ciModule.isPresent()) {
+            CIModule foundCIModule = ciModule.get();
+
+            foundCIModule.setName(inputDto.getName());
+            foundCIModule.setType(inputDto.getType());
+            foundCIModule.setPrice(inputDto.getPrice());
+
+            CIModule updatedCIModule = ciModuleRepository.save(foundCIModule);
+
+            return transferToDto(updatedCIModule);
+        } else {
+            throw new RecordNotFoundException("No CI-module found with ID: " + id);
+        }
+    }
+
+    public CIModuleDto patchCIModule(Long id, CIModuleInputDto inputDto) {
+        Optional<CIModule> ciModule = ciModuleRepository.findById(id);
+
+        if (ciModule.isPresent()) {
+            CIModule foundCIModule = ciModule.get();
+
+            if (inputDto.getName() != null) {
+                foundCIModule.setName(inputDto.getName());
+            }
+            if (inputDto.getType() != null) {
+                foundCIModule.setType(inputDto.getType());
+            }
+            if (inputDto.getPrice() != null) {
+                foundCIModule.setPrice(inputDto.getPrice());
+            }
+
+
+            CIModule patchedCIModule = ciModuleRepository.save(foundCIModule);
+
+            return transferToDto(patchedCIModule);
+        } else {
+            throw new RecordNotFoundException("No CI-module found with ID: " + id);
+        }
     }
 
 
