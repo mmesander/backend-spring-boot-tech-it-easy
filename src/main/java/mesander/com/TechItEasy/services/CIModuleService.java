0db0a558eaaf119b7d0dbbc32b23a1dcf15d6c86
Mark Mesander <mark_mesander@hotmail.com>
2023-12-12 11:08:07 +0100

    update transfer methods & add constructor injection

--- src/main/java/mesander/com/TechItEasy/services/CIModuleService.java
+++ src/main/java/mesander/com/TechItEasy/services/CIModuleService.java
@@ -3,21 +3,37 @@ package mesander.com.TechItEasy.services;
 import mesander.com.TechItEasy.dtos.input.CIModuleInputDto;
 import mesander.com.TechItEasy.dtos.output.CIModuleDto;
 import mesander.com.TechItEasy.models.CIModule;
+import mesander.com.TechItEasy.repositories.CIModuleRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CIModuleService {
+    private final CIModuleRepository ciModuleRepository;
+
+    public CIModuleService(CIModuleRepository ciModuleRepository) {
+        this.ciModuleRepository = ciModuleRepository;
+    }
+
+
     // Transfer Methods
-    private Long id;
-
-    private String name;
-    private String type;
-    private Double price;
-
     public CIModule transferToCIModule(CIModuleInputDto dto) {
         CIModule ciModule = new CIModule();
 
+        ciModule.setName(dto.getName());
+        ciModule.setType(dto.getType());
+        ciModule.setPrice(dto.getPrice());
 
+        return ciModule;
     }
-    public CIModuleDto transferToDto(CIModule ciModule) {
 
+    public CIModuleDto transferToDto(CIModule ciModule) {
+        CIModuleDto dto = new CIModuleDto();
+
+        dto.setId(ciModule.getId());
+        dto.setName(ciModule.getName());
+        dto.setType(ciModule.getType());
+        dto.setPrice(ciModule.getPrice());
+
+        return dto;
     }
 }
