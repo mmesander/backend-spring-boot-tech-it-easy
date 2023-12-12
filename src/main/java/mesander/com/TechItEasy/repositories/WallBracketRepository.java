b9dc751c3ddc50e0723c6a21903c00d0d10e40ea
Mark Mesander <mark_mesander@hotmail.com>
2023-12-12 11:29:00 +0100

    update wall bracket repository

--- src/main/java/mesander/com/TechItEasy/repositories/WallBracketRepository.java
+++ src/main/java/mesander/com/TechItEasy/repositories/WallBracketRepository.java
@@ -3,5 +3,8 @@ package mesander.com.TechItEasy.repositories;
 import mesander.com.TechItEasy.models.WallBracket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WallBracketRepository extends JpaRepository<WallBracket, Long> {
+    List<WallBracket> findWallBracketsByAdjustable(Boolean adjustable);
 }
