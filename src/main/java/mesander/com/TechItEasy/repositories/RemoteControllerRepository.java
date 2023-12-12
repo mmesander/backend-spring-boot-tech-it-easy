8275925af94ed4274a6445e7c6d98dbedc9e7dd6
Mark Mesander <mark_mesander@hotmail.com>
2023-12-12 09:31:41 +0100

    add Repositories

---
+++ src/main/java/mesander/com/TechItEasy/repositories/RemoteControllerRepository.java
@@ -0,0 +1,7 @@
+package mesander.com.TechItEasy.repositories;
+
+import mesander.com.TechItEasy.models.RemoteController;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RemoteControllerRepository extends JpaRepository<RemoteController, Long> {
+}
---
+++ src/main/java/mesander/com/TechItEasy/repositories/WallBracketRepository.java
@@ -0,0 +1,7 @@
+package mesander.com.TechItEasy.repositories;
+
+import mesander.com.TechItEasy.models.WallBracket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WallBracketRepository extends JpaRepository<WallBracket, Long> {
+}
