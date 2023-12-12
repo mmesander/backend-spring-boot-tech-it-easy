a49c1e9ea7ed6f59bab9eae40dc37157754b0b8d
Mark Mesander <mark_mesander@hotmail.com>
2023-12-12 18:03:55 +0100

    add data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,36 @@
+-- Televisions
 ALTER SEQUENCE televisions_seq INCREMENT BY 1;
+INSERT INTO televisions (id, type, brand, name, price, available_size, refresh_rate, screen_type, screen_quality,
+                         smart_tv, wifi, voice_control, hdr, bluetooth, ambi_light, original_stock, sold)
+VALUES
+    (nextval('televisions_seq'), 'NH3216SMART', 'Nikkei', 'HD smart TV', 159, 32, 100, 'LED', 'HD ready',  true, true, false, false, false, false, 235885, 45896),
+    (nextval('televisions_seq'), '43PUS6504/12/L', 'Philips', '4K UHD LED Smart Tv', 379, 43, 60, 'LED', 'Ultra HD',  true, true, false, true, false, false, 8569452, 5685489),
+    (nextval('televisions_seq'), '43PUS6504/12/M', 'Philips', '4K UHD LED Smart Tv', 379, 50, 60, 'LED', 'Ultra HD',  true, true, false, true, false, false, 345549, 244486),
+    (nextval('televisions_seq'), '43PUS6504/12/S', 'Philips', '4K UHD LED Smart Tv', 379, 58, 60, 'LED', 'Ultra HD',  true, true, false, true, false, false, 6548945, 4485741),
+    (nextval('televisions_seq'), 'OLED55C16LA', 'LG', 'LG OLED55C16LA', 989, 55, 100, 'OLED', 'ULTRA HD',  true, true, true, true, true, false, 50000, 20500),
+    (nextval('televisions_seq'), 'OLED55C16LA', 'LG', 'LG OLED55C16LA', 989, 55, 100, 'OLED', 'ULTRA HD',  true, true, true, true, true, false, 50000, 20500);
 
-INSERT INTO televisions (id, type, brand, name, price, available_size, refresh_rate, screen_type, screen_quality, smart_tv, wifi, voice_control, hdr, bluetooth, ambi_light, original_stock, sold) VALUES (nextval('televisions_seq'), 'NH3216SMART', 'Nikkei', 'HD smart TV', 159, 32, 100, 'LED', 'HD ready',  true, true, false, false, false, false, 235885, 45896);
-INSERT INTO televisions (id, type, brand, name, price, available_size, refresh_rate, screen_type, screen_quality, smart_tv, wifi, voice_control, hdr, bluetooth, ambi_light, original_stock, sold) VALUES (nextval('televisions_seq'), '43PUS6504/12/L', 'Philips', '4K UHD LED Smart Tv', 379, 43, 60, 'LED', 'Ultra HD',  true, true, false, true, false, false, 8569452, 5685489);
-INSERT INTO televisions (id, type, brand, name, price, available_size, refresh_rate, screen_type, screen_quality, smart_tv, wifi, voice_control, hdr, bluetooth, ambi_light, original_stock, sold) VALUES (nextval('televisions_seq'), '43PUS6504/12/M', 'Philips', '4K UHD LED Smart Tv', 379, 50, 60, 'LED', 'Ultra HD',  true, true, false, true, false, false, 345549, 244486);
-INSERT INTO televisions (id, type, brand, name, price, available_size, refresh_rate, screen_type, screen_quality, smart_tv, wifi, voice_control, hdr, bluetooth, ambi_light, original_stock, sold) VALUES (nextval('televisions_seq'), '43PUS6504/12/S', 'Philips', '4K UHD LED Smart Tv', 379, 58, 60, 'LED', 'Ultra HD',  true, true, false, true, false, false, 6548945, 4485741);
-INSERT INTO televisions (id, type, brand, name, price, available_size, refresh_rate, screen_type, screen_quality, smart_tv, wifi, voice_control, hdr, bluetooth, ambi_light, original_stock, sold) VALUES (nextval('televisions_seq'), 'OLED55C16LA', 'LG', 'LG OLED55C16LA', 989, 55, 100, 'OLED', 'ULTRA HD',  true, true, true, true, true, false, 50000, 20500);
-INSERT INTO televisions (id, type, brand, name, price, available_size, refresh_rate, screen_type, screen_quality, smart_tv, wifi, voice_control, hdr, bluetooth, ambi_light, original_stock, sold) VALUES (nextval('televisions_seq'), 'OLED55C16LA', 'LG', 'LG OLED55C16LA', 989, 55, 100, 'OLED', 'ULTRA HD',  true, true, true, true, true, false, 50000, 20500);
 
+-- Remote Controllers
 ALTER SEQUENCE remote_controllers_seq INCREMENT BY 1;
-INSERT INTO remote_controllers(id, compatible_with, battery_type, name, brand, price, original_stock) VALUES (nextval('remote_controllers_seq'), 'rowan', 'AAA', 'bert', 'Philips', 801.99, 2);
+INSERT INTO remote_controllers(id, compatible_with, battery_type, name, brand, price, original_stock)
+VALUES
+    (nextval('remote_controllers_seq'), 'Philips', 'AAA', 'PH10103', 'Philips', 22.99, 2),
+    (nextval('remote_controllers_seq'), 'LG', 'AAA', 'LG1130', 'LG', 28.39, 8);
+
+
+-- CI-Modules
+ALTER SEQUENCE ci_modules_seq INCREMENT BY 1;
+INSERT INTO ci_modules(id, name, type, price)
+VALUES
+    (nextval('ci_modules_seq'), 'universal CI-module', 'ZZXT300', 89.99);
+
+
+-- Wall Brackets
+INSERT INTO wall_brackets (id, size, adjustable, name, price)
+VALUES
+    (1001, '25X32', false, 'LG bracket', 32.23),
+    (1002, '25X32/32X40', true, 'LG bracket', 32.23),
+    (1003, '25X25', false, 'Philips bracket', 32.23),
+    (1004, '25X32/32X40', true, 'Nikkei bracket', 32.23),
+    (1005, '25X32', false, 'Nikkei bracket', 32.23);
