6a85357b1c589c57e9328b0127df2c3b782ba70d
Mark Mesander <mark_mesander@hotmail.com>
2023-12-12 11:01:13 +0100

    add function in repository

--- src/main/java/mesander/com/TechItEasy/repositories/CIModuleRepository.java
+++ src/main/java/mesander/com/TechItEasy/repositories/CIModuleRepository.java
@@ -3,5 +3,8 @@ package mesander.com.TechItEasy.repositories;
 import mesander.com.TechItEasy.models.CIModule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CIModuleRepository extends JpaRepository<CIModule, Long> {
+    List<CIModule> findCIModuleByName(String name);
 }
