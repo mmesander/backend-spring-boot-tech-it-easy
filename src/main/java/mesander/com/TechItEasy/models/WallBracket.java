50f3c255ef0524336005b41caa8c46a2395c2824
Mark Mesander <mark_mesander@hotmail.com>
2023-12-12 09:20:27 +0100

    add wallBracket

--- src/main/java/mesander/com/TechItEasy/models/WallBracket.java
+++ src/main/java/mesander/com/TechItEasy/models/WallBracket.java
@@ -1,4 +1,38 @@
 package mesander.com.TechItEasy.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "wall_brackets")
 public class WallBracket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String size;
+    private Boolean adjustable;
+    private String name;
+    private Double price;
+
+
+    // Constructors
+    public WallBracket() {
+    }
+
+    public WallBracket(
+            Long id,
+            String size,
+            Boolean adjustable,
+            String name,
+            Double price
+    ) {
+        this.id = id;
+        this.size = size;
+        this.adjustable = adjustable;
+        this.name = name;
+        this.price = price;
+    }
 }
