35ec08da3e438e1c5eaf628addeb4f9bf12ba2f2
Mark Mesander <mark_mesander@hotmail.com>
2023-12-12 11:03:55 +0100

    add transfer methods

--- src/main/java/mesander/com/TechItEasy/services/CIModuleService.java
+++ src/main/java/mesander/com/TechItEasy/services/CIModuleService.java
@@ -1,4 +1,23 @@
 package mesander.com.TechItEasy.services;
 
+import mesander.com.TechItEasy.dtos.input.CIModuleInputDto;
+import mesander.com.TechItEasy.dtos.output.CIModuleDto;
+import mesander.com.TechItEasy.models.CIModule;
+
 public class CIModuleService {
+    // Transfer Methods
+    private Long id;
+
+    private String name;
+    private String type;
+    private Double price;
+
+    public CIModule transferToCIModule(CIModuleInputDto dto) {
+        CIModule ciModule = new CIModule();
+
+
+    }
+    public CIModuleDto transferToDto(CIModule ciModule) {
+
+    }
 }
